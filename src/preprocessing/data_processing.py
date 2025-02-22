8faf669e4954d58dde3b804a1cf069d9996262bf
Szakacs Daniel <daniel.sokach@gmail.com>
2025-02-22 16:02:33 +0100

    data processing has been updated

--- src/preprocessing/data_processing.py
+++ src/preprocessing/data_processing.py
@@ -5,27 +5,37 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, StandardScaler, PowerTransformer
 import joblib
 
+
 def encode_sex_column(df):
   df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
   return df
 
-sex_transformer = FunctionTransformer(encode_sex_column, validate=False)
-cat_transformer = OneHotEncoder(drop="first")
-num_transformer = Pipeline([
-    ("power_trans", PowerTransformer(method='yeo-johnson')),
-    ("std_scaler", StandardScaler())
-])
+def create_pipeline(df):
+  sex_transformer = FunctionTransformer(encode_sex_column, validate=False)
+  cat_transformer = OneHotEncoder(drop="first")
+  num_transformer = Pipeline([
+      ("power_trans", PowerTransformer(method='yeo-johnson')),
+      ("std_scaler", StandardScaler())
+  ])
 
-preprocessor = ColumnTransformer([
-    ("embarked", cat_transformer, ["Embarked"]),
-    ("sex", sex_transformer, ["Sex"]),
-    ("num_features", num_transformer, ["Fare", "Age"]),
+  preprocessor = ColumnTransformer([
+      ("embarked", cat_transformer, ["Embarked"]),
+      ("sex", sex_transformer, ["Sex"]),
+      ("num_features", num_transformer, ["Fare", "Age"]),
+  ])
 
-])
+  pipeline = Pipeline([
+      ("preprocessing", preprocessor)
+      ])
+  print("Pipeline created")
 
-pipeline = Pipeline([
-    ("preprocessing", preprocessor)
-    ])
+  df_transformed = pipeline.fit_transform(df)
+  print("Pipeline trained")  
 
-df_transformed = pipeline.fit_transform(df_for_pipeline)
-joblib.dump(pipeline, "models/preprocessing_pipeline.pkl")
+  print("Save pipeline")
+  joblib.dump(df_transformed, "preprocessing_pipeline.pkl")
+  return df_transformed
+
+def load_pipeline(path):
+  pipeline_loaded = joblib.load(path)
+  return pipeline_loaded
